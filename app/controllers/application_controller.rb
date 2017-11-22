59712764310cc25989eae9302b911b60b93ce82c
lozovsky <lozovsky93@gmail.com>
2017-11-23 00:47:20 +0100

    Flash message after valid signup added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,4 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello"
   end
-
-private
-
-  def sprawdz_logowanie
-		unless session[:uzytkownik_id]
-			#flash[:notice] = "Jestes niezalogowany"
-			redirect_to(:controller => 'dostep', :action => 'login')
-			return false
-		else
-			return true
-		end
-  end
 end
