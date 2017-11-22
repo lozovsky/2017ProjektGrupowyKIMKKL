4fbb70a67904c47a6ea40422dbf2bb3c2d0d2721
zyng <kisielewskimichal@gmail.com>
2017-11-22 12:29:27 +0100

    create Uzytkownik table and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171122111714) do
+
+  create_table "uzytkowniks", force: :cascade do |t|
+    t.string "imie", limit: 20
+    t.string "nazwisko", limit: 50
+    t.string "uzytkownik", limit: 25
+    t.string "email", limit: 100, default: "", null: false
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["uzytkownik"], name: "index_uzytkowniks_on_uzytkownik"
+  end
 
 end
