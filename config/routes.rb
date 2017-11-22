966b9adad54a737ead8f7eef556471a3bb64d018
lozovsky <lozovsky93@gmail.com>
2017-11-22 23:28:12 +0100

    show user profiles on page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
 
-
-
-  get 'users/new'
-  get 'admin', :to => "dostep#index"
-  
   root 'static_pages#home'
+
   get  '/help',    to: 'static_pages#help'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  match ':controller(/:action(/:id))', :via => [:get, :post]
+
+  resources :users
 end
