28464710a21870a1132aeb108daf7e638fdf24c0
lozovsky <lozovsky93@gmail.com>
2017-11-22 22:49:07 +0100

    password validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   def setup
     @empty_field_eror = "Pole nie może być puste!"
     @nickname_length_error = "Pole musi zawierać od 4 do 20 znaków!"
+    @password_length_error = "Pole musi zawierać minimum 6 znaków!"
     @over_50_error = "Pole musi zawierać maksymalnie 50 znaków!"
     @over_255_error = "Pole musi zawierać maksymalnie 255 znaków!"
     @invalid_email_regex_error = "Błędny format adresu email!"
@@ -11,6 +12,8 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save { email.downcase! }
   
+  has_secure_password
+
   validates :nickname, 
     presence: { in: true, message: ": #{@empty_field_eror}" },
     length: { maximum: 20, minimum: 4, message: ": #{@nickname_length_error}" },
@@ -29,5 +32,10 @@ class User < ApplicationRecord
     length: { maximum: 255, message: ": #{@over_255_error}" },
     format: { with: VALID_EMAIL_REGEX, message: ": #{@invalid_email_regex_error}" },
     uniqueness: { case_sensitive: false, message: ": #{@field_not_unique_error}"}
+
+  validates :password,
+    presence: { in: true, message: ": #{@empty_field_eror}" },
+    length: { minimum: 6, message: ": #{@password_length_error}" }
+
 
 end
