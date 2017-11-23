0237be2e96900cc6910cf905b79c218260d1a42e
lozovsky <lozovsky93@gmail.com>
2017-11-23 04:34:29 +0100

    Require login #1

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:one)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: {
+      user: {
+        nickname: "",
+        name: "",
+        surname: "",
+        email: "foo@invalid",
+        password: "foo",
+        password_confirmation: "bar"
+      }
+    }
+    assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    nickname = "trolololo"
+    name = "Trolo"
+    surname = "Lolo"
+    email = "trolo@lolo.com"
+    patch user_path(@user), params: {
+      user: {
+        nickname: nickname,
+        name: name,
+        surname: surname,
+        email: email,
+        password: "",
+        password_confirmation: ""
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal nickname, @user.nickname
+    assert_equal name, @user.name
+    assert_equal surname, @user.surname
+    assert_equal email, @user.email
+  end
+end
