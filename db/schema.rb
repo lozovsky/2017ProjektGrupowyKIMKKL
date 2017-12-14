2b2bf705375f544b1ff837e334cf880fda399fd6
zyng <kisielewskimichal@gmail.com>
2017-12-14 14:08:15 +0100

    add models for: line,distance,route,timetable and stop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123045845) do
+ActiveRecord::Schema.define(version: 20171214130553) do
+
+  create_table "distances", force: :cascade do |t|
+    t.integer "from_stop_id"
+    t.integer "to_stop_id"
+    t.integer "travel_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["from_stop_id", "to_stop_id"], name: "index_distances_on_from_stop_id_and_to_stop_id", unique: true
+    t.index ["from_stop_id"], name: "index_distances_on_from_stop_id"
+    t.index ["to_stop_id"], name: "index_distances_on_to_stop_id"
+  end
+
+  create_table "lines", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.integer "line_id"
+    t.integer "distance_id"
+    t.integer "stop_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["line_id", "stop_number"], name: "index_routes_on_line_id_and_stop_number", unique: true
+    t.index ["line_id"], name: "index_routes_on_line_id"
+  end
+
+  create_table "stops", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "timetables", force: :cascade do |t|
+    t.integer "line_id"
+    t.time "departure_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["line_id"], name: "index_timetables_on_line_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nickname"
